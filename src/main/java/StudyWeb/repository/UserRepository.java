a557d616226bc4354785c1faeaeb14a9d5b71322
WOONBE <kepull2918@naver.com>
2023-05-22 15:01:15 +0900

    유저 서비스 추가

--- src/main/java/StudyWeb/repository/UserRepository.java
+++ src/main/java/StudyWeb/repository/UserRepository.java
@@ -3,5 +3,9 @@ package StudyWeb.repository;
 import StudyWeb.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+
 }
--- src/main/java/StudyWeb/service/UserService.java
+++ src/main/java/StudyWeb/service/UserService.java
@@ -1,4 +1,46 @@
 package StudyWeb.service;
 
+import StudyWeb.domain.User;
+import StudyWeb.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long join(User user){
+
+        userRepository.save(user);
+        return user.getId();
+
+    }
+
+    //유저 중복 검사
+    private void validateDuplicateUser(User user){
+        Optional<User> findUsers = userRepository.findById(user.getId());
+        if(!findUsers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    //전체 유저 조회
+    public List<User> findUsers(){
+        return userRepository.findAll();
+    }
+
+
+
+
+
+
+
+
 }
